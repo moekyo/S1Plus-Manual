d9cea26384f737b2388ba5e503338b3d89c20daf
moekyo <shigotods@outlook.com>
2025-09-07 19:41:15 +0800

    feat(nav): 加粗手动同步按钮的SVG图标
    通过为该图标的 <path> 元素添加 `stroke` 和 `stroke-width` 属性，
    在视觉上增加了其轮廓厚度。
    
    这提高了图标在高分辨率屏幕上的清晰度和可辨识度，优化了用户体验。

--- S1Plus.js
+++ S1Plus.js
@@ -2334,7 +2334,8 @@
         li.id = 's1p-nav-sync-btn';
         const a = document.createElement('a');
         a.href = 'javascript:void(0);';
-        a.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 4C9.25144 4 6.82508 5.38626 5.38443 7.5H8V9.5H2V3.5H4V5.99936C5.82381 3.57166 8.72764 2 12 2C17.5228 2 22 6.47715 22 12H20C20 7.58172 16.4183 4 12 4ZM4 12C4 16.4183 7.58172 20 12 20C14.7486 20 17.1749 18.6137 18.6156 16.5H16V14.5H22V20.5H20V18.0006C18.1762 20.4283 15.2724 22 12 22C6.47715 22 2 17.5228 2 12H4Z"></path></svg>`;
+        // --- [MODIFIED] 在 <path> 元素中添加了 stroke 和 stroke-width 属性来加粗图标 ---
+        a.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path stroke="currentColor" stroke-width="0.4" d="M12 4C9.25144 4 6.82508 5.38626 5.38443 7.5H8V9.5H2V3.5H4V5.99936C5.82381 3.57166 8.72764 2 12 2C17.5228 2 22 6.47715 22 12H20C20 7.58172 16.4183 4 12 4ZM4 12C4 16.4183 7.58172 20 12 20C14.7486 20 17.1749 18.6137 18.6156 16.5H16V14.5H22V20.5H20V18.0006C18.1762 20.4283 15.2724 22 12 22C6.47715 22 2 17.5228 2 12H4Z"></path></svg>`;
         li.appendChild(a);
 
         if (settings.syncDirectChoiceMode) {
