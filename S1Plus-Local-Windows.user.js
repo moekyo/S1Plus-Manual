71149e44abaa7d2eae8a15f7ba0809dad22338d3
moekyo <shigotods@outlook.com>
2026-04-12 16:12:35 +0800

    feat: add local loaders for macOS and Windows with updated scripts

--- S1Plus-Local-Windows.user.js
+++ S1Plus-Local-Windows.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         S1 Plus (Local)
+// @name         S1 Plus (Local Windows)
 // @namespace    http://tampermonkey.net/
 // @version      99.9.10
-// @description  本地开发版，直接加载磁盘文件
+// @description  本地开发版，直接加载磁盘文件（Windows）
 // @author       Antigravity
 // @match        https://stage1st.com/2b/*
-// @require      file:///Users/rexxin/Development/S1Plus-Manual/S1Plus.js
+// @require      file:///D:/MyZone/S1Plus-Manual/S1Plus.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
@@ -19,5 +19,5 @@
 
 (function () {
   "use strict";
-  console.log("[S1 Plus] 本地加载器已启动");
+  console.log("[S1 Plus] Windows 本地加载器已启动");
 })();
