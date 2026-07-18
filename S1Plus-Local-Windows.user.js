0bae02c625f531fd12e6e5e446a9330c63f96d4d
moekyo <shigotods@outlook.com>
2026-07-18 17:37:56 +0800

    build: align Windows loader grants with userscript

--- S1Plus-Local-Windows.user.js
+++ S1Plus-Local-Windows.user.js
@@ -2,7 +2,7 @@
 // @name         S1 Plus (Local Windows)
 // @namespace    http://tampermonkey.net/
 // @version      99.9.10
-// @description  本地开发版，直接加载磁盘文件（Windows）
+// @description  本地开发版；Phase 0 直接加载根 S1Plus.js，Phase 0.5 后切换到 dist 预览产物（Windows）
 // @author       Antigravity
 // @match        https://stage1st.com/2b/*
 // @require      file:///D:/MyZone/S1Plus-Manual/S1Plus.js
@@ -14,6 +14,7 @@
 // @grant        GM_openInTab
 // @grant        GM_download
 // @grant        GM_addValueChangeListener
+// @grant        GM_removeValueChangeListener
 // @grant        GM_listValues
 // @connect      *
 // @run-at       document-start
